8c72ebc01432f9394670fc619f51ae82c5310206
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-08-11 12:13:11 +0200

    Create todos module and import it into app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { join } from 'path';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { join } from 'path';
         path: join(process.cwd(), 'src/graphql.ts'),
       },
     }),
+    TodoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
