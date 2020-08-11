862b5f0410817cade8e5a2361359d356b9083eaf
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-08-12 01:40:42 +0200

    Create findAll todo function returning an array of todos

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,15 +1,18 @@
 import { Resolver, Query } from '@nestjs/graphql';
+import { TodoService } from './todo.service';
+import { Todo } from './todo.schema';
 
 @Resolver('Todo')
 export class TodoResolver {
+  constructor(private todoService: TodoService) {}
+
   @Query()
   hello(): string {
     return 'Hello world';
   }
 
   @Query()
-  todos(): [] {
-    return [];
+  async todos(): Promise<Todo[]> {
+    return await this.todoService.findAll();
   }
-  //Should return an array of todos
 }
